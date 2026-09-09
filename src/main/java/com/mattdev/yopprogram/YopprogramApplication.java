4f2bc27cf34acb3dca1db679b4a41c74c9949598
Mendoza Matias <mendoza.e.matias@gmail.com>
2026-09-08 22:17:11 -0300

    first commit

---
+++ src/main/java/com/mattdev/yopprogram/YopprogramApplication.java
@@ -0,0 +1,13 @@
+package com.mattdev.yopprogram;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YopprogramApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YopprogramApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mattdev/yopprogram/YopprogramApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mattdev.yopprogram;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YopprogramApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
